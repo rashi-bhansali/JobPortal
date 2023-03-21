f67fc7f1aface36833a5be428a8dd813fe2e5df8
Rashi Bhansali <52106856+rashi-bhansali@users.noreply.github.com>
2023-03-21 22:58:44 +0530

    Create Counter.js

---
+++ src/Components/Data/Counter.js
@@ -0,0 +1,20 @@
+const counter = [
+	{
+		value:"42",
+		desc:"Jobs Posted"
+	},
+	{
+		value:"20",
+		desc:"Jobs Filled"
+	},
+	{
+		value:"67",
+		desc:"Companies"
+	},
+	{
+		value:"80",
+		desc:"Members"
+	}
+];
+
+export default counter; 
