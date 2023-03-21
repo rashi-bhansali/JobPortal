d1864a32478643028fc33c2993ae8b9b538c4eb9
Rashi Bhansali <52106856+rashi-bhansali@users.noreply.github.com>
2023-03-21 22:59:31 +0530

    creating files for fetching dummy data

---
+++ src/Components/Data/Job.js
@@ -0,0 +1,89 @@
+const jobs=[
+	{
+		id: 1, 
+		logo: "https://dofonline.co.uk/wp-content/uploads/2018/07/Deutsche-Bank-logo.jpg",
+		role: "Data Analyst",
+		company: "Deutsche Bank",
+		loc: "Pune, Maharashtra"
+	},
+	{
+		id: 2, 
+		logo: "https://i2.wp.com/techsyndrome.in/wp-content/uploads/2018/01/nvidia-logo-square.png.imgw_.960.540.jpg?",
+		role: "System Software Engineer",
+		company: "Nvidia",
+		loc: "Bangalore, India"
+	},
+	{
+		id: 3, 
+		logo: "https://pipap.sprep.org/sites/default/files/credit_suisse.jpg",
+		role: "Web Designer / Developer",
+		company: "Credit Suisse",
+		loc: "Pune, Maharashtra"
+	},
+	{
+		id: 4, 
+		logo: "https://s.abcnews.com/images/Technology/ht_google_lb_150901_16x9_992.jpg",
+		role: "Data Scientist",
+		company: "Google",
+		loc: "Hyderabad, India"
+	},
+	{
+		id: 5, 
+		logo: "https://gbatemp.net/attachments/184731/",
+		role: "Marketing Director",
+		company: "Amazon",
+		loc: "Mumbai, Maharashtra"
+	},
+	{
+		id: 6, 
+		logo: "https://www.ccsinsight.com/images/blog/2018/03/IBM_logo_l.png",
+		role: "UI / UX Designer",
+		company: "IBM",
+		loc: "Pune, Maharashtra"
+	},
+	{
+		id: 7, 
+		logo: "http://allvectorlogo.com/img/2017/03/deutsche-bank-logo.png",
+		role: "Data Analyst",
+		company: "Deutsche Bank",
+		loc: "Pune, Maharashtra"
+	},
+	{
+		id: 8, 
+		logo: "https://i2.wp.com/techsyndrome.in/wp-content/uploads/2018/01/nvidia-logo-square.png.imgw_.960.540.jpg?",
+		role: "System Software Engineer",
+		company: "Nvidia",
+		loc: "Bangalore, India"
+	},
+	{
+		id: 9, 
+		logo: "https://pipap.sprep.org/sites/default/files/credit_suisse.jpg",
+		role: "Web Designer / Developer",
+		company: "Credit Suisse",
+		loc: "Pune, Maharashtra"
+	},
+	{
+		id: 10, 
+		logo: "https://s.abcnews.com/images/Technology/ht_google_lb_150901_16x9_992.jpg",
+		role: "Data Scientist",
+		company: "Google",
+		loc: "Hyderabad, India"
+	},
+	{
+		id: 11, 
+		logo: "https://gbatemp.net/attachments/184731/",
+		role: "Marketing Director",
+		company: "Amazon",
+		loc: "Mumbai, Maharashtra"
+	},
+	{
+		id: 12, 
+		logo: "https://www.ccsinsight.com/images/blog/2018/03/IBM_logo_l.png",
+		role: "UI / UX Designer",
+		company: "IBM",
+		loc: "Pune, Maharashtra"
+	}
+];
+
+
+export default jobs;
---
+++ src/Components/Data/Reviews.js
@@ -0,0 +1,46 @@
+const ReviewsList = [
+    {
+        id : '1',
+        name : "Robert",
+        skill: "Web Designer",
+        imgsrc: "https://images.unsplash.com/photo-1595152772835-219674b2a8a6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
+        msg: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+        id : '2',
+        name : "Stacy",
+        skill: "Data Scientist",
+        imgsrc: "https://images.unsplash.com/photo-1597223557154-721c1cecc4b0?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NDZ8fGZhY2UlMjBoYXBweXxlbnwwfDJ8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
+        msg: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+        id : '3',
+        name : "Peter",
+        skill: "Human Resources",
+        imgsrc: "https://images.unsplash.com/photo-1605857840732-188f2f08cb31?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTE3fHxmYWNlfGVufDB8MnwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
+        msg: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+        id : '4',
+        name : "Robert",
+        skill: "Web Designer",
+        imgsrc: "https://images.unsplash.com/photo-1595152772835-219674b2a8a6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
+        msg: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+        id : '5',
+        name : "Stacy",
+        skill: "Data Scientist",
+        imgsrc: "https://images.unsplash.com/photo-1597223557154-721c1cecc4b0?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NDZ8fGZhY2UlMjBoYXBweXxlbnwwfDJ8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
+        msg: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+        id : '6',
+        name : "Peter",
+        skill: "Human Resources",
+        imgsrc: "https://images.unsplash.com/photo-1605857840732-188f2f08cb31?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTE3fHxmYWNlfGVufDB8MnwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
+        msg: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    }
+]
+
+export default ReviewsList;
